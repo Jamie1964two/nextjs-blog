cda3f175fd10066b6eb8d4a3811acd7a96ae1e06
Jamie Moreland <jamie1964two@hotmail.co.uk>
2023-01-02 18:05:47 +0000

    remove lottie

--- components/LogoMini.js
+++ components/LogoMini.js
@@ -1,72 +1,64 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { gsap } from "gsap/dist/gsap";
+import { useRef, useLayoutEffect } from "react";
 import Link from "next/link";
-import animationData from "../public/images/miniLogo.json";
-import Lottie from "react-lottie";
-
-//import * as LottiePlayer from "@lottiefiles/lottie-player";
 
 const LogoMini = () => {
-  // const [lottie, setLottie] = useState(<></>);
+  const miniLogoRef = useRef();
+  const tl = useRef();
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      tl.current = gsap.timeline({
+        delay: 0.2,
+        repeat: -1, // number of repeats (-1 for infinite)
+        repeatDelay: 10,
+      });
 
-  // useEffect(() => {
-  //   setLottie(() => {
-  //     return (
-  //       <Lottie
-  //         options={{
-  //           loop: false,
-  //           autoplay: true,
-  //           animationData: animationData,
-  //           rendererSettings: {
-  //             preserveAspectRatio: "xMinYMin ",
-  //           },
-  //         }}
-  //         height={48}
-  //         width={84}
-  //       />
-  //     );
-  //   }, []);
-  // });
+      tl.current
+        .from(".div1", {
+          x: 50,
+          duration: 0.8,
+          opacity: 0.1,
+          ease: "power4.out",
+        })
+        .from(
+          ".div2",
+          {
+            x: 50,
+            duration: 0.5,
+            opacity: 0.1,
+            ease: "power4.out",
+          },
+          "-=80%"
+        )
+        .from(
+          ".div3",
+          {
+            x: 50,
+            duration: 0.5,
+            opacity: 0.1,
+            ease: "power4.out",
+          },
+          "-=20%"
+        );
+    }, miniLogoRef);
+
+    return () => ctx.revert();
+  }, []);
 
   return (
     <div className="flex justify-start  lg:flex-1 hover:opacity-70 ">
-      {/* <div className="max-w-[30px] max-h-[30px] border-2 border-blue-500"> */}
-      <Link href="/">
-        {/* <a href="#"> */}
-        {/* <span className="sr-only">City Church Choucester</span>
-        <div className="bg-myblue rounded-tl-3xl rounded-br-3xl border-2 border-white text-white text-xl font-sans px-4 py-2 hover:opacity-50">
-          {/* g-gradient-to-br from-[#5b98b9] via-[#98c8e1] via-[#83adc3]
-          to-[#3981a7] */}
-        {/* CCC */}
-        {/* </div> */}
-        {/* </a> */}
-        {/* <Lottie
-          options={{
-            loop: false,
-            autoplay: true,
-            animationData: animationData,
-            rendererSettings: {
-              preserveAspectRatio: "xMinYMin ",
-            },
-          }}
-          height={45}
-          width={180}
-        /> */}
-        <Lottie
-          options={{
-            loop: false,
-            autoplay: true,
-            animationData: animationData,
-            rendererSettings: {
-              preserveAspectRatio: "xMinYMin ",
-            },
-          }}
-          height={48}
-          width={84}
-        />
+      <Link href="/" className="focus:outline-none">
+        <div
+          ref={miniLogoRef}
+          className="bg-gradient-to-br from-[#5b98b9] via-[hsl(201,55%,64%)] to-[#83adc3] rounded-tl-3xl rounded-br-3xl border-2 border-white text-white text-xl font-sans px-4 py-2 hover:opacity-50 font-medium"
+        >
+          <div className="div1 inline-block">C</div>
+          <div className="div2 inline-block">C</div>
+          <div className="div3 inline-block">G</div>
+        </div>
       </Link>
     </div>
-    // </div>
   );
 };
 
